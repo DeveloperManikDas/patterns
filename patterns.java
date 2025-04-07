d7625ec53ea0b10ae8c39f8c7c9004b53b139cfb
DeveloperManikDas <developermanikdas@gmail.com>
2025-04-07 11:34:48 +0530

    add inverted right-angled triangle and half number pyramid

--- patterns.java
+++ patterns.java
@@ -12,6 +12,26 @@ class patternPrinter {
         }
     }
 
+    // This is pattern of inverted Right-Angle Triangle
+    public static void printInvertedRightAngledTriangle(int n) {
+        for (int line = 1; line <= n; line++) {
+            for (int star = 4; star >= line; star--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    // This is pattern of Half Pyramid with numbers
+    public static void halfPyramidNumbers(int n) {
+        for (int line = 1; line <= n; line++) {
+            for (int num = 1; num <= line; num++) {
+                System.out.print(num);
+            }
+            System.out.println("");
+        }
+    }
+
 }
 
 class patterns {
@@ -22,8 +42,8 @@ class patterns {
         do {
             System.out.println("\n===== Pattern Printer =====");
             System.out.println("1. Right-Angled Triangle");
-            System.out.println("2. Square");
-            System.out.println("3. Inverted Triangle");
+            System.out.println("2. Inverted Right-Angled Triangle");
+            System.out.println("3. Half-Pyramids with Numbers");
             System.out.println("4. Exit");
             System.out.print("Choose a pattern (1-4): ");
             choice = sc.nextInt();
@@ -40,7 +60,12 @@ class patterns {
                 case 1:
                     patternPrinter.printRightAngledTriangle(n);
                     break;
-
+                case 2:
+                    patternPrinter.printInvertedRightAngledTriangle(n);
+                    break;
+                case 3:
+                    patternPrinter.halfPyramidNumbers(n);
+                    break;
                 default:
                     break;
             }
