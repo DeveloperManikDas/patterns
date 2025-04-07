86955c6b84cbbb39679e7c3ebfe07e908ceb7211
DeveloperManikDas <developermanikdas@gmail.com>
2025-04-07 11:02:42 +0530

    add modularity

--- patterns.java
+++ patterns.java
@@ -1,19 +1,52 @@
-import java.util.*; 
+import java.util.*;
 
-class patterns{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in); 
-        System.out.print("Please enter the line: "); 
-        // int n = sc.nextInt(); 
-        int n = 4; 
-        System.out.println("I need to print "+ n + " lines."); 
+class patternPrinter {
 
-        for(int line = 1; line<= n; line++){
-            for(int star=1; star <= line; star++){
-                System.out.print("*"); 
+    // This is pattern of Right-Angle Triangle
+    public static void printRightAngledTriangle(int n) {
+        for (int line = 1; line <= n; line++) {
+            for (int star = 1; star <= line; star++) {
+                System.out.print("*");
             }
-            System.out.println(""); 
+            System.out.println("");
         }
-        sc.close(); 
+    }
+
+}
+
+class patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, choice;
+
+        do {
+            System.out.println("\n===== Pattern Printer =====");
+            System.out.println("1. Right-Angled Triangle");
+            System.out.println("2. Square");
+            System.out.println("3. Inverted Triangle");
+            System.out.println("4. Exit");
+            System.out.print("Choose a pattern (1-4): ");
+            choice = sc.nextInt();
+
+            if (choice == 4) {
+                System.out.println("Keep Going... Never stop coding!");
+                break;
+            }
+
+            System.out.print("Enter the number of lines: ");
+            n = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    patternPrinter.printRightAngledTriangle(n);
+                    break;
+
+                default:
+                    break;
+            }
+
+        } while (true);
+
+        sc.close();
     }
 }
